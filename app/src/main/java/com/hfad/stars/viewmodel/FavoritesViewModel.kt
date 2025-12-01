b51ef6537b8366cb67ded3983bc9c131d80714a3
AlexandraKhodykina <89029524057@mail.ru>
2025-12-01 20:58:11 +0700

    добавление удаления

--- app/src/main/java/com/hfad/stars/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/com/hfad/stars/viewmodel/FavoritesViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.hfad.stars.model.CosmicObject
 import com.hfad.stars.repository.CosmicRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 
 class FavoritesViewModel(application: Application) : AndroidViewModel(application) {
@@ -16,10 +18,26 @@ class FavoritesViewModel(application: Application) : AndroidViewModel(applicatio
     private val _isEmpty = MutableLiveData<Boolean>()
     val isEmpty: LiveData<Boolean> = _isEmpty
 
+    private val _deleteSuccess = MutableLiveData<Boolean>()
+    val deleteSuccess: LiveData<Boolean> = _deleteSuccess
+
     init {
         // Наблюдаем за изменением избранных
         favorites.observeForever { objects ->
             _isEmpty.value = objects.isEmpty()
         }
     }
+    // Функция удаления объекта
+    fun deleteFavorite(cosmicObject: CosmicObject) {
+        viewModelScope.launch {
+            try {
+                // Можно просто снять отметку "избранное"
+                repository.toggleFavorite(cosmicObject.id, true)
+                _deleteSuccess.value = true
+            } catch (e: Exception) {
+                _deleteSuccess.value = false
+            }
+        }
+    }
+
 }
