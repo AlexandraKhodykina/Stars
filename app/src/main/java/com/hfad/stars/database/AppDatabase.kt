9aff2b5a84a75dc67db7efb88d2da0f81c006a32
AlexandraKhodykina <89029524057@mail.ru>
2025-12-01 16:05:50 +0700

    Простая база данных Room

---
+++ app/src/main/java/com/hfad/stars/database/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.hfad.stars.database
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.hfad.stars.model.CosmicObject
+
+
+@Database(entities = [CosmicObject::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun cosmicObjectDao(): CosmicObjectDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "stars.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
